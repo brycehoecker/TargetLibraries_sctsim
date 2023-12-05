30841004ac4addf55c97bf053ab9fde4e6600869
brycehoecker <brycehoecker@gmail.com>
2023-12-05 15:31:03 -0800

    2023-12-05 15:31:03

--- targetdriver-issue37423/src/RawEvent.cc
+++ targetdriver-issue37423/src/RawEvent.cc
@@ -2,79 +2,73 @@
 #include "TargetDriver/RawEvent.h"
 
 namespace CTA {
-namespace TargetDriver {
+	namespace TargetDriver {
 
-double RawEvent::fTimeoutSec = 0.3;
+		double RawEvent::fTimeoutSec = 0.3;
 
-RawEvent::RawEvent(uint16_t pNPacketsPerEvent, uint16_t pPacketSize)
-    : fTimedOut(false) {
-  Clear();
-  // fDataPackets.resize(pNPacketsPerEvent);
-  // for (auto it = fDataPackets.begin(); it != fDataPackets.end(); ++it) {
-  //   *it = std::make_shared<CTA::TargetDriver::DataPacket>(pPacketSize);
-  // }
-  fDataPackets.reserve(pNPacketsPerEvent);
-  for (uint32_t i = 0; i < pNPacketsPerEvent; ++i) {
-    fDataPackets.push_back(new DataPacket(pPacketSize));
-  }  // i
-}
+		RawEvent::RawEvent(uint16_t pNPacketsPerEvent, uint16_t pPacketSize) : fTimedOut(false) {
+			Clear();
+			// fDataPackets.resize(pNPacketsPerEvent);
+			// for (auto it = fDataPackets.begin(); it != fDataPackets.end(); ++it) {
+			//   *it = std::make_shared<CTA::TargetDriver::DataPacket>(pPacketSize);
+			// }
+			fDataPackets.reserve(pNPacketsPerEvent);
+			for (uint32_t i = 0; i < pNPacketsPerEvent; ++i) {
+				fDataPackets.push_back(new DataPacket(pPacketSize));
+			}//for
+		}//RawEvent::RawEvent
 
-/// Initialize the internal states and clear data packets' flag.
-void RawEvent::Clear() {
-  fEventHeader.Init();
-  fTimedOut = false;
-  fFlushed = false;
-  fRead = false;
-  fWaveformCheckStatus = true;
+		/// Initialize the internal states and clear data packets' flag.
+		void RawEvent::Clear() {
+			fEventHeader.Init();
+			fTimedOut = false;
+			fFlushed = false;
+			fRead = false;
+			fWaveformCheckStatus = true;
+			for (auto it = fDataPackets.begin(); it != fDataPackets.end(); ++it) {
+				(*it)->ClearFilledFlag();
+			}//for
+		}//void RawEvent::Clear
 
-  for (auto it = fDataPackets.begin(); it != fDataPackets.end(); ++it) {
-    (*it)->ClearFilledFlag();
-  }
-}
+		/// Add a new data packet (uint8_t array) to the packet vector. The data is
+		/// memcopied to the vector.
+		bool RawEvent::AddNewPacket(const uint8_t* data, uint16_t packetID,	uint16_t packetSize, bool checkflag) {
+			if (packetID >= fDataPackets.size()) {
+				std::cerr << "Bad Packet ID: " << packetID;
+				return false;
+			}//if
 
-/// Add a new data packet (uint8_t array) to the packet vector. The data is
-/// memcopied to the vector.
-bool RawEvent::AddNewPacket(const uint8_t* data, uint16_t packetID,
-                            uint16_t packetSize, bool checkflag) {
-  if (packetID >= fDataPackets.size()) {
-    std::cerr << "Bad Packet ID: " << packetID;
-    return false;
-  }
+			DataPacket* packet = fDataPackets[packetID];
+			// TODO(Akira): check for overwrite?
 
-  DataPacket* packet = fDataPackets[packetID];
-  // TODO(Akira): check for overwrite?
+			bool filled = packet->Fill(data, packetSize);
+			/// bool filled = true;
 
-  bool filled = packet->Fill(data, packetSize);
-  /// bool filled = true;
+			if (filled) {
+				fEventHeader.IncrementNPacketsFilled();
+				if (fEventHeader.GetNPacketsFilled() == 1) {
+					// Is this really OK to write the time stamp at this point?
+					// usec order delay is expected after the actual arrival time of the first
+					// packet
+					fEventHeader.SetTimeStampNow();
+					fEventHeader.SetTACK(packet->GetTACKTime());
+				}
+				if (checkflag) {
+					Waveform* w = packet->GetWaveform(0);
+					float rms, mean;
+					w->GetMeanAndRMS(mean, rms, 10);
+					///      std::cout << "Check: " << mean << " " << rms << std::endl;
+					fWaveformCheckStatus = ((mean < 900.0) && (rms < 100.0));
+					//TODO(Bryce): check if below is running correctly (cerr isn't "IN" the if statement)
+					if (!fWaveformCheckStatus)
+					std::cerr << "RawEvent::FillPacket - Suspicious waveform - mean " << mean << " rms " << rms << std::endl;
+				}
+			} else {
+				std::cerr << "RawEvent::FillPacket - failed to fill packet ID: " << packetID 
+				<< " size was " << packetSize << " expected " << packet->GetPacketSize() << " - event ID " << fEventHeader.GetEventID();
+			}
 
-  if (filled) {
-    fEventHeader.IncrementNPacketsFilled();
-    if (fEventHeader.GetNPacketsFilled() == 1) {
-      // Is this really OK to write the time stamp at this point?
-      // usec order delay is expected after the actual arrival time of the first
-      // packet
-      fEventHeader.SetTimeStampNow();
-      fEventHeader.SetTACK(packet->GetTACKTime());
-    }
-    if (checkflag) {
-      Waveform* w = packet->GetWaveform(0);
-      float rms, mean;
-      w->GetMeanAndRMS(mean, rms, 10);
-      ///      std::cout << "Check: " << mean << " " << rms << std::endl;
-      fWaveformCheckStatus = ((mean < 900.0) && (rms < 100.0));
-      if (!fWaveformCheckStatus)
-        std::cerr << "RawEvent::FillPacket - Suspicious waveform - mean "
-                  << mean << " rms " << rms << std::endl;
-    }
-  } else {
-    std::cerr << "RawEvent::FillPacket - failed to fill packet ID: " << packetID
-              << " size was " << packetSize << " expected "
-              << packet->GetPacketSize() << " - event ID "
-              << fEventHeader.GetEventID();
-  }
-
-  return filled;
-}
-
-}  // namespace TargetDriver
-}  // namespace CTA
+			return filled;
+		}//bool RawEvent::AddNewPacket
+	}// namespace TargetDriver
+}// namespace CTA
